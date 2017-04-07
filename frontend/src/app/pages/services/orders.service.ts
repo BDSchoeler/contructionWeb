6fa7a3bd8cb92bcb1635ab783142ab0496a34f9f
lori-dalkin <lori.dalkin@gmail.com>
2017-04-07 01:48:12 -0400

    Order Stuff

--- frontend/src/app/pages/services/orders.service.ts
+++ frontend/src/app/pages/services/orders.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Response }          from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
- 
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 @Injectable()
-export class AuthenticationService {
+
+export class OrdersService {
     constructor(private http: Http) { }
  
-    login(username: string, password: string) {
-        return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
-            .map((response: Response) => {
-                // login successful if there's a jwt token in the response
-                let user = response.json();
-                if (user && user.token) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
-                }
-            });
+    getOrdersByProjectID(id){
+        return this.http.get('http://localhost:8080/orders/project/' + id).toPromise()
+                  .then(this.extractData)
+                  .catch(this.handleError);
+}
+
+
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || { };
+  }
+    private handleError (error: Response | any) {
+    
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
     }
- 
-    logout() {
-        // remove user from local storage to log user out
-        localStorage.removeItem('currentUser');
-    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
 }

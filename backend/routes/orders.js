9c603dc9419c41bc08db4a8fb034d924c6016c65
Bryce <darktarro100@gmail.com>
2017-04-07 01:38:22 -0400

    added order route

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var Order=require('../models/order');
 
-router.get('/product/:id',function(req,res,next){
+router.get('/project/:id',function(req,res,next){
 if(req.params.id){
 
-    Order.getOrderByProductId(req.params.id,function(err,rows){
+    Order.getOrderByProjectId(req.params.id,function(err,rows){
 
         if(err)
         {

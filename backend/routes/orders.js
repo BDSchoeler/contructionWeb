89a27362c259b8b6329e979f49380ea912544ca6
lori-dalkin <lori.dalkin@gmail.com>
2017-04-10 01:22:10 -0400

    Frontend for add phase and project

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -67,7 +67,7 @@ router.post('/',function(req,res,next){
                     res.json(req.body);//or return count for 1 & 0
             }
         });
-    }
+    });
 });
 router.post('/:id',function(req,res,next){
   Order.deleteAll(req.body,function(err,count){
